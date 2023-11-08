71e7d96840b1f32f0731234ce6b283ed0895769a
manojkumarns <82507560+manojkumarns@users.noreply.github.com>
2023-11-08 09:17:57 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treats all the js code as the newer version
+
+//alert(3+3) //we are using node.js not browser
+
+let name ="manoj"
+let age =23
+let isLoggedIn= false
+
+//number=>2 to power of 53 
+//bigint 
+//String =>
+//boolean => true/false
+//null => Standalone value
+//undefined => 
+//symbol =>unique
+
+//object
+
+console.log(typeof "manoj")
+
--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,17 @@ let accountEmail = "manoj@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
 
-accountId= 2
+//accountId= 2
+
+accountEmail ="hc@hc.com"
+accountPassword="214521"
+accountCity= "banglore"
+
+console.table([ accountId,accountEmail,accountPassword, accountCity])
+
+/*
+Prefered not to use war
+because of issue in block scope and functional scope
+*/
 
 console.log(accountId);
