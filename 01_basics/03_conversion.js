31af46add7c539649ddce5602f46738ffc02c08b
manojkumarns <82507560+manojkumarns@users.noreply.github.com>
2023-11-08 10:08:43 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,22 @@
+let score ="Manoj"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumeber =Number(score)
+console.log(typeof valueInNumeber);
+console.log(valueInNumeber)
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 ; false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let sumeNumber =33
+let StringNumber = String(sumeNumber)
+console.log(StringNumber)
